0d3c667172c1ad7a34a7bf89a32256dfc918601f
Diego Noriega <Diego.ale036@gmail.com>
2025-09-28 16:56:52 -0500

    Validation created but need a few tweaks

--- main.js
+++ main.js
@@ -13,17 +13,49 @@ function colorSlider() {
 
 colorSlider();
 
-// const generateBtn = document.querySelector("#generate-btn");
+const generateBtn = document.querySelector("#generate-btn");
 
-//     generateBtn.addEventListener('click', e => {
-//         e.preventDefault();
-//         validGen();
-//     });
+    generateBtn.addEventListener('click', e => {
+        e.preventDefault();
 
-// function validGen() {
+        validGen();
+    });
 
-// }
+function genPassWord(rawValPass) {
 
-// function genPassWord() {
-//     console.log();
-// }
+
+}
+
+function validGen() {
+
+    const checkForEach = document.querySelectorAll('.checks');
+    const errorMSG = document.querySelector("#error-checkbox");
+    const reallyTXT = document.querySelector("#really");
+
+    let isValid = true;
+
+        checkForEach.forEach(check => {
+
+            if (check.checked === false) {
+                errorMSG.style.visibility = "visible";
+                
+                isValid = false;                
+
+            }
+            if (slider.value < 3) {
+                reallyTXT.style.visibility = "visible";
+                
+                isValid = false;
+
+            }
+            if (check.checked) {
+                errorMSG.style.visibility = "hidden";
+                reallyTXT.style.visibility = "hidden";
+                
+                isValid = true;
+
+            }
+
+        return (isValid === true) ? genPassWord(check) : isValid;
+        });
+}
